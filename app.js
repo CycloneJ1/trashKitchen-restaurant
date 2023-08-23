2ed53c37e67014e03d4a17ab3c0d5462f4a1df4b
Cyclonej <cyclonejmoto@gmail.com>
2023-08-23 14:08:41 +0200

    lots added as contact ,homepage,layout,product

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 
 
-// Make everything inside of public/ available
-app.use(express.static('public'));
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+
+app.use(express.static('public')); // Make everything inside of public/ available
 
 
 
@@ -12,15 +15,93 @@ app.use(express.static('public'));
 
 // Home Page
 app.get("/", (req, res, next) => {
-    res.sendFile(__dirname + '/views/homepage.html');
+    res.render('homepage');
 });
 
 
 // Contact Page
 app.get("/contact", (req, res, next) => {
-    res.sendFile(__dirname + '/views/contact.html');
+    res.render('contact');
 })
 
+
+app.get("/pizzas/margarita", (req, res, next) => {
+    // res.render(path, data);
+
+    const dataMargarita = {
+        title: 'Pizza Margarita',
+        price: 12,
+        recommendedDrink: 'beer',
+        imageFile: 'pizzaMar.jpeg',
+        ingredients: [
+            {
+                ingredientName: "mozzarella",
+                calories: 400
+            },
+            {
+                ingredientName: "tomato sauce",
+                calories: 200
+            },
+            {
+                ingredientName: "basilicum",
+                calories: 30
+            },
+          ],
+    };
+
+    res.render("product", dataMargarita)
+})
+
+
+app.get("/pizzas/veggie", (req, res, next) => {
+    const dataVeggie = {
+        title: 'Veggie Pizza',
+        price: 15,
+        recommendedDrink: 'power smoothie',
+        imageFile: 'pizzaVeg.jpeg',
+        ingredients: [
+            {
+                ingredientName: "cherry tomatoes",
+                calories: 400
+            },
+            {
+                ingredientName: "basilicum",
+                calories: 200
+            },
+            {
+                ingredientName: "olives",
+                calories: 30
+            },
+          ],
+    }; 
+    res.render("product", dataVeggie);
+})
+
+
+app.get("/pizzas/seafood", (req, res, next) => {
+    const dataSeafood = {
+        title: 'Seafood Pizza',
+        price: 20,
+        recommendedDrink: 'white wine',
+        imageFile: 'pizzaSea.jpeg',
+        ingredients: [
+            {
+                ingredientName: "",
+                calories: 400
+            },
+            {
+                ingredientName: "",
+                calories: 200
+            },
+            {
+                ingredientName: "",
+                calories: 30
+            },
+          ],
+    };
+      
+    res.render("product", dataSeafood);
+})
 
 
 
